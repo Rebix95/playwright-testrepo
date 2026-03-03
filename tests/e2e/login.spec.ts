fa85f39de34c8909c1a40707d0958180b39f23ac
Stefan Harlaar <stefan.harlaar@heeyoo.nl>
2026-03-03 11:05:27 +0100

    Updated login testfile

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -4,4 +4,10 @@ test('login', async ({ page }) => {
     await page.goto('https://ecommerce-playground.lambdatest.io/');
     await page.getByRole('button', { name: 'My account' }).hover();
     await page.getByTitle('Login').click();
+    await page.getByPlaceholder('Email address').click();
+    await page.getByPlaceholder('Email address').fill('stefanharlaar@gmail.com');
+    await page.getByPlaceholder('Password').click();
+    await page.getByPlaceholder('Password').fill('Test123');
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page).toHaveURL('https://ecommerce-playground.lambdatest.io/index.php?route=account/account');
 });
