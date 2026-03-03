e5ff8942ff6ede2aadb55254688376018a86e485
Stefan Harlaar <stefan.harlaar@heeyoo.nl>
2026-03-03 14:41:58 +0100

    a test: refactor login test
    - fix login test
    - delete example test

--- tests/e2e/login.spec.ts
+++ tests/e2e/login.spec.ts
@@ -3,9 +3,9 @@ import { test, expect } from '@playwright/test';
 test('login', async ({ page }) => {
     await page.goto('https://ecommerce-playground.lambdatest.io/');
     await page.getByRole('button', { name: 'My account' }).hover();
-    await page.getByTitle('Login').click();
-    await page.getByPlaceholder('Email address').click();
-    await page.getByPlaceholder('Email address').fill('stefanharlaar@gmail.com');
+    await page.locator('span.title:has-text("Login")').click();
+    await page.getByPlaceholder('E-Mail Address').click();
+    await page.getByPlaceholder('E-Mail Address').fill('stefanharlaar@gmail.com');
     await page.getByPlaceholder('Password').click();
     await page.getByPlaceholder('Password').fill('Test123');
     await page.getByRole('button', { name: 'Login' }).click();
