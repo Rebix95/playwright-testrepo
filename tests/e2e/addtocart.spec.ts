61c124aaf4788c2259a45ccf095952d035cd6c57
Stefan Harlaar <stefan.harlaar@heeyoo.nl>
2026-03-09 13:42:35 +0100

    test: add new steps to testcase

--- tests/e2e/addtocart.spec.ts
+++ tests/e2e/addtocart.spec.ts
@@ -1,2 +1,11 @@
 import { test, expect } from '@playwright/test';
 
+test('add to cart', async ({ page }) => {
+    // Go to homepage
+    await page.goto('https://ecommerce-playground.lambdatest.io/');
+
+    // Click Add to Cart button on iMac product
+    await page.getByRole('link', { name: 'iMac' }).hover();
+    await page.getByRole('button', { name: /Add to Cart/i }).click();
+
+});
